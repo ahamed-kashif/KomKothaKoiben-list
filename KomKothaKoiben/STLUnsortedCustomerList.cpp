82de5db6ab00a03195df01f30f5ac7c10f70342f
Salauddin Ahamed Kashif <sahkashif@hotmail.com>
2018-11-04 04:38:44 +0600

    had problem on inserting customer object in stl forward_list

--- KomKothaKoiben/STLUnsortedCustomerList.cpp
+++ KomKothaKoiben/STLUnsortedCustomerList.cpp
@@ -4,14 +4,63 @@
 
 STLUnsortedCustomerList::STLUnsortedCustomerList()
 {
+	make_empty();
 }
 
 
 STLUnsortedCustomerList::~STLUnsortedCustomerList()
 {
+	make_empty();
 }
 
-bool STLUnsortedCustomerList::put_customer_details(Customer customer)
+int STLUnsortedCustomerList::get_length()
+{
+	return length;
+}
+
+void STLUnsortedCustomerList::make_empty()
+{
+
+	customerList.clear();
+
+	length = 0;
+}
+
+bool STLUnsortedCustomerList::is_empty()
+{
+	return (customerList.empty());
+}
+
+Customer STLUnsortedCustomerList::get_customer_details(int index)
+{
+	while (!customerList.empty())
+	{
+		if (customerList.front().get_customer_index() == index)
+			return customerList.front();
+	}
+	cout << "Customer Index Number does not match";
+	return customerList.front();
+}
+
+void STLUnsortedCustomerList::put_customer_details(Customer customer)
 {
 	customerList.push_front(customer);
+}
+
+/*bool STLUnsortedCustomerList::delete_customer_details(Customer customer)
+{
+	
+}*/
+
+
+
+Customer STLUnsortedCustomerList::get_next_customer()
+{
+	return customerList.front();
+}
+
+void STLUnsortedCustomerList::print_customer_list()
+{
+	while (customerList.empty())
+		customerList.front().print_customer_deatails();
 }
--- KomKothaKoiben/STLUnsortedCustomerList.h
+++ KomKothaKoiben/STLUnsortedCustomerList.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include<exception>
 #include <forward_list>
 #include "Customer.h"
 #include <string>
@@ -11,19 +12,19 @@ class STLUnsortedCustomerList
 {
 private:
 	forward_list<Customer> customerList;
+	int length;
 public:
 	STLUnsortedCustomerList();
 	~STLUnsortedCustomerList();
-	bool is_full();
+	//bool is_full();
 	int get_length();
 	void make_empty();
 	bool is_empty();
 	Customer get_customer_details(int);
-	bool put_customer_details(Customer);
-	bool delete_customer_details(int);
-	//Iterator Operations
-	void reset_list_customer_list();
-	bool has_next_customer();
+	void put_customer_details(Customer);
+	//bool delete_customer_details(Customer);
+	
+	//bool has_next_customer();
 	Customer get_next_customer();
 	//Printing List Contents
 	void print_customer_list();

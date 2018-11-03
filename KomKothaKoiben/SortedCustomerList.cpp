6bec1259632f2e571d74955e14e66f642f512754
Salauddin Ahamed Kashif <sahkashif@hotmail.com>
2018-11-04 02:36:21 +0600

    SortedCustomerList class is created which will work as sorted link list if Customer object

---
+++ KomKothaKoiben/SortedCustomerList.cpp
@@ -0,0 +1,130 @@
+#include<exception>
+#include<iostream>
+#include "SortedCustomerList.h"
+
+
+
+SortedCustomerList::SortedCustomerList()
+{
+	make_empty();
+}
+
+
+SortedCustomerList::~SortedCustomerList()
+{
+	make_empty();
+}
+
+
+
+
+
+bool SortedCustomerList::is_full()
+{
+	try {
+		NodeType* newNode = new NodeType;
+		delete newNode;
+	}
+	catch (std::bad_alloc exception) {
+		return true;
+	}
+	return false;
+}
+
+int SortedCustomerList::get_length()
+{
+	return length;
+}
+
+void SortedCustomerList::make_empty()
+{
+	customerList = nullptr;
+	currentPosition = nullptr;
+	length = 0;
+}
+
+bool SortedCustomerList::is_empty()
+{
+	return (length == 0 && customerList == nullptr);
+}
+
+Customer SortedCustomerList::get_customer_details(int customIndex)
+{
+	NodeType* curr = customerList;
+	bool found = false;
+	while (curr != nullptr && !found) {
+		if (curr->info.get_customer_index() != customIndex)
+			curr = curr->next;
+		else {
+			found = true;
+			return curr->info;
+		}
+	}
+	found = false;
+	cout << endl;
+	cout << "Customer Index is not correct!!" << endl;
+	return curr->info;
+}
+
+bool SortedCustomerList::put_customer_details(Customer customer)
+{
+
+
+}
+
+bool SortedCustomerList::delete_customer_details(int index)
+{
+	NodeType* curr = customerList;
+	NodeType* prev = nullptr;
+	bool found = false;
+	while (curr != nullptr && !found) {
+		if (curr->info.get_customer_index() == index) {
+			found = true;
+			if (prev == nullptr) {
+				customerList = curr->next;
+			}
+			else {
+				prev->next = curr->next;
+			}
+			delete curr;
+			length--;
+		}
+		else {
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+	return found;
+
+}
+
+void SortedCustomerList::reset_list_customer_list()
+{
+	currentPosition = nullptr;
+}
+
+bool SortedCustomerList::has_next_customer()
+{
+	if (currentPosition == nullptr)
+		return (customerList != nullptr);
+
+	return (currentPosition->next != nullptr);
+}
+
+Customer SortedCustomerList::get_next_customer()
+{
+	currentPosition = ((currentPosition == nullptr) ? customerList : currentPosition->next);
+	return currentPosition->info;
+
+}
+
+void SortedCustomerList::print_customer_list()
+{
+	for (int index = 0; index < length; index++)
+	{
+		if(has_next_customer())
+		get_next_customer().print_customer_deatails();
+	}
+
+}
+
---
+++ KomKothaKoiben/SortedCustomerList.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Customer.h"
+#include<string>
+
+using namespace std;
+
+
+class SortedCustomerList
+{
+public:
+
+	struct NodeType {
+		Customer info = Customer();
+		NodeType* next = nullptr;
+	};
+
+public:
+	SortedCustomerList();
+	~SortedCustomerList();
+	bool is_full();
+	int get_length();
+	void make_empty();
+	bool is_empty();
+	Customer get_customer_details(int);
+	bool put_customer_details(Customer);
+	bool delete_customer_details(int);
+	//Iterator Operations
+	void reset_list_customer_list();
+	bool has_next_customer();
+	Customer get_next_customer();
+	//Printing List Contents
+	void print_customer_list();
+private:
+	NodeType* customerList = nullptr;
+	int length = 0;
+	NodeType* currentPosition = nullptr;
+};
+
